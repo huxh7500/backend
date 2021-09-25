4bc878c75b4131c5eaffe80428bd45b2dfcb6e29
huxh7500 <86956846+huxh7500@users.noreply.github.com>
2021-09-25 22:01:39 +0300

    new update

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ if(process.env.NODE_ENV === 'production'){
 app.use(notFound)
 app.use(errorHandler)
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 80
 app.listen(
     PORT, 
     console.log(`Backend is running in ${process.env.NODE_ENV} mode on port: ${PORT}`)
